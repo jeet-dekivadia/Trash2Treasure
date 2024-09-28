b83146a182af5893002fff90d8c86ebc94658533
Jeet Dekivadia <jeet.university@gmail.com>
2024-09-27 22:41:36 -0400

    Create Rewards.js

---
+++ frontend/src/components/Rewards.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Rewards() {
+    return (
+        <div>
+            <h1>Your Rewards</h1>
+            <p>Check out your rewards here! This section can include details about what rewards are available and how to redeem them.</p>
+            <ul>
+                <li>Eco-Friendly Water Bottle</li>
+                <li>Discount Coupons</li>
+                <li>Donation to Environmental Causes</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Rewards;
